cc8fc57873ecb19d36c69bfa156aaac9375997b6
Christian Merrick <192653753+cM-2024-12@users.noreply.github.com>
2025-09-06 15:56:32 -0400

    JS Exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function sumAll (numA, numB) {
+    let total = 0
+    const min = Math.min(numA, numB)
+    const max = Math.max(numA, numB)
+    const rng = max - min  // You don't actually need this variable
+    for (let i = min; i <= max; i++) {
+        total += i  // Fixed: += instead of =+
+    }
+    return total
 };
+console.log(sumAll(5,10))
 
-// Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
